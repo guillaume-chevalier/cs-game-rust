a435d85cb5de1c1671ec7c0e66e4a1ff79a0d1a2
Nathaniel <nathaniel.simard.1@ulaval.ca>
2018-02-04 12:47:30 -0500

    Add problem method add on matrix

---
+++ matrix/README.md
@@ -0,0 +1,7 @@
+# Problem
+
+You have to declare and implement the `add` method that can make the test pass. To make it more fun, don't use brackets `[]` to access or change any values. The file to change is `matrix.rs`.
+
+To execute all the tests just run `cargo test`.
+
+Good luck !
--- matrix/src/matrix.rs
+++ matrix/src/matrix.rs
@@ -39,49 +39,6 @@ pub fn from_vec(vec: Vec<Vec<f64>>) -> Matrix {
     }
 }
 
-impl Matrix {
-    pub fn set(&mut self, i: usize, j: usize, value: f64) {
-        self.table[i][j] = value;
-    }
-
-    pub fn get(&self, i: usize, j: usize) -> f64 {
-        self.table[i][j]
-    }
-
-    pub fn mul_with_index(&self, other: &Matrix) -> Matrix {
-        if self.size.1 != other.size.0 {
-            panic!(
-                "Matrix have wrong dimensions ({},{}), not compatible with ({},{})",
-                self.size.0, self.size.1, other.size.0, other.size.1
-            );
-        }
-
-        let mut new_matrix = new(self.size.0, self.size.1, 0.0);
-
-        for i in 0..self.size.0 {
-            for j in 0..other.size.1 {
-                for k in 0..other.size.0 {
-                    new_matrix.table[i][j] += self.table[i][k] + other.table[k][j];
-                }
-            }
-        }
-
-        return new_matrix;
-    }
-
-    pub fn mul_without_index(&self, other: &Matrix) -> Matrix {
-        if self.size.1 != other.size.0 {
-            panic!(
-                "Matrix have wrong dimensions ({},{}), not compatible with ({},{})",
-                self.size.0, self.size.1, other.size.0, other.size.1
-            );
-        }
-
-        let new_matrix = new(self.size.0, self.size.1, 0.0);
-        return new_matrix;
-    }
-}
-
 impl Display for Matrix {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut output = String::new();
@@ -106,31 +63,37 @@ mod tests {
     use matrix;
 
     #[test]
-    fn test_set() {
-        let mut matrix = matrix![[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]];
-        matrix.set(0, 0, 1.0);
-        assert_eq!(1.0, matrix.get(0, 0));
-    }
-
-    #[test]
-    fn test_mul_without_index() {
+    fn test_add_without_index() {
         let table = vec![
-            (
-                matrix![[5.0, 4.0, 13.5], [10.5, 2.0, 8.0]],
-                matrix![[15.0, 7.0, 155.0], [17.0, 3.0, 16.0], [16.5, 3.5, 0.0]],
-            ),
+            (matrix![[2.0], [1.5]], 2.5, matrix![[4.5], [4.0]]),
             (
                 matrix![[2.0, 5.0, 3.0], [2.0, 4.0, 10.0], [10.0, 0.0, 6.0]],
-                matrix![[8.0, 4.0, 2.0], [9.0, 1.0, 15.0], [14.0, 3.0, 16.0]],
+                1.0,
+                matrix![[3.0, 6.0, 4.0], [3.0, 5.0, 11.0], [11.0, 1.0, 7.0]],
             ),
-            (matrix![[5.0, 4.0, 13.5]], matrix![[15.0], [17.0], [16.5]]),
+            (
+                matrix![
+                    [2.0, 5.0, 3.0, -9.0],
+                    [2.0, 4.0, 10.0, -10.0],
+                    [10.0, 0.0, 6.0, 0.0]
+                ],
+                -1.0,
+                matrix![
+                    [1.0, 4.0, 2.0, -10.0],
+                    [1.0, 3.0, 9.0, -11.0],
+                    [9.0, -1.0, 5.0, -1.0]
+                ],
+            ),
         ];
 
-        for matrix in table {
-            let actual_matrix = matrix.0.mul_without_index(&matrix.1);
-            let expected_matrix = matrix.0.mul_with_index(&matrix.1);
+        for value in table {
+            let mut matrix = value.0;
+            let value_to_add = value.1;
+            let expected_matrix = value.2;
 
-            assert_eq!(expected_matrix, actual_matrix);
+            matrix.add(value_to_add);
+
+            assert_eq!(expected_matrix, matrix);
         }
     }
 }
